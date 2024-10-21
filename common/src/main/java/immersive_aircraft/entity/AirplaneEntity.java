b15dc05cdf9de307332bada3b103289fda003a89
Bomb787 <39660651+Bomb787@users.noreply.github.com>
2024-10-20 21:08:28 -0400

    Stuff :D
    Added method overrides in AirplaneEntitiy.
    Reverted my mesh changes as they break non-Blockbench models.

--- common/src/main/java/immersive_aircraft/entity/AirplaneEntity.java
+++ common/src/main/java/immersive_aircraft/entity/AirplaneEntity.java
@@ -1,14 +1,19 @@
 package immersive_aircraft.entity;
 
 import immersive_aircraft.item.upgrade.VehicleStat;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3f;
 
 /**
  * Implements airplane like physics properties and accelerated towards
  */
 public abstract class AirplaneEntity extends AircraftEntity {
+
+    protected double aoa;
+
     public AirplaneEntity(EntityType<? extends AircraftEntity> entityType, Level world, boolean canExplodeOnCrash) {
         super(entityType, world, canExplodeOnCrash);
     }
@@ -30,12 +35,55 @@ public abstract class AirplaneEntity extends AircraftEntity {
     }
 
     @Override
+    protected void convertPower(Vec3 direction) {
+        Vec3 velocity = getDeltaMovement();
+        double drag = Math.abs(direction.dot(velocity.normalize()));
+        setDeltaMovement(velocity.normalize()
+                .lerp(direction, getProperties().get(VehicleStat.LIFT))
+                .scale(velocity.length() * (drag * getProperties().get(VehicleStat.FRICTION) + (1.0 - getProperties().get(VehicleStat.FRICTION)))));
+    }
+
+    @Override
+    protected void updateVelocity() {
+        // get direction
+        Vector3f direction = getForwardDirection();
+
+        // glide
+        float diff = (float) (lastY - getY());
+        if (lastY != 0.0 && getProperties().get(VehicleStat.GLIDE_FACTOR) > 0 && diff != 0.0) {
+            setDeltaMovement(getDeltaMovement().add(toVec3d(direction).scale(diff * getProperties().get(VehicleStat.GLIDE_FACTOR) * (1.0f - Math.abs(direction.y)))));
+        }
+        lastY = (float) getY();
+
+        // convert power
+        convertPower(toVec3d(direction));
+
+        // friction
+        applyFriction();
+
+        if (onGround()) {
+            // Landing
+            setXRot((getXRot() + getProperties().get(VehicleStat.GROUND_PITCH)) * 0.9f - getProperties().get(VehicleStat.GROUND_PITCH));
+        } else {
+            // Wind
+            Vector3f effect = getWindEffect();
+            setXRot(getXRot() + effect.x);
+            setYRot(getYRot() + effect.z);
+
+            float offsetStrength = 0.005f;
+            setDeltaMovement(getDeltaMovement().add(effect.x * offsetStrength, 0.0f, effect.z * offsetStrength));
+        }
+    }
+
+    @Override
     protected void updateController() {
         if (!isVehicle()) {
             return;
         }
 
         super.updateController();
+
+        this.aoa = -this.getXRot() - (Mth.RAD_TO_DEG * Math.asin(this.getDeltaMovement().y() / this.getDeltaMovement().length()));
 
         // engine control
         if (movementY != 0) {
@@ -47,6 +95,12 @@ public abstract class AirplaneEntity extends AircraftEntity {
 
         // get direction
         Vector3f direction = getForwardDirection();
+
+        /*double a = -this.getXRot();
+        double b = Mth.RAD_TO_DEG * Math.asin(this.getDeltaMovement().y() / this.getDeltaMovement().length());
+        System.out.println("pitch: " + a);
+        System.out.println("movement: " + b);
+        System.out.println("aoa: " + (a - b));*/
 
         // speed
         float thrust = (float) (Math.pow(getEnginePower(), 2.0) * getProperties().get(VehicleStat.ENGINE_SPEED));
